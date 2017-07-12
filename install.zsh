ef6a77b6ebb652d64b40589c089b788f12fd00b9
Benjamin Pang <benjinpang@gmail.com>
2017-07-11 20:40:01 -0400

    oops

--- install.zsh
+++ install.zsh
@@ -4,6 +4,8 @@ mv ~/.zshrc ~/.zshrc_old
 mv ~/.vimrc ~/.vimrc_old
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone git://github.com/zdharma/fast-syntax-highlighting ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 [[ $1 == 'm' ]] && (
