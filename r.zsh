1310d8ac0d08eaccd06b464c262bb444eae86156
Benjamin Pang <benjinpang@gmail.com>
2017-07-12 17:53:31 -0400

    not sure why common aliases plugin not there

--- r.zsh
+++ r.zsh
@@ -11,7 +11,7 @@ HYPHEN_INSENSITIVE="true"
 
 export KEYTIMEOUT=10
 export EDITOR=vim
-plugins=(git fast-syntax-highlighting zsh-autosuggestions z history colored-man-pages archlinux extract)
+plugins=(common-aliases git fast-syntax-highlighting zsh-autosuggestions z history colored-man-pages archlinux extract)
 export ZSH=$HOME/.oh-my-zsh
 source $ZSH/oh-my-zsh.sh
 source ~/zsh-git-prompt/zshrc.sh
