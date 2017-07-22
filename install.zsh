f4e511868f51cb94a616ee1b53793f169a556877
Benjamin Pang <benjinpang@gmail.com>
2017-07-22 12:07:35 -0400

    tmux

--- install.zsh
+++ install.zsh
@@ -7,7 +7,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone git://github.com/olivierverdier/zsh-git-prompt
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone git://github.com/zdharma/fast-syntax-highlighting ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "source-file ~/vimrc/r.tmux" > ~/.tmux.conf
 
 [[ $1 == 'm' ]] && (
   echo "source ~/vimrc/mr.vim" > ~/.vimrc
